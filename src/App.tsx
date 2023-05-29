24d9a4f9be733ac6f21b6f149d972f451491a06f
Nguyen Duong <muoi07052001@gmail.com>
2023-05-29 20:58:02 +0700

    Add shop pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Shops from './pages/Shops';
 
 function App() {
-  return <div className="flex items-center justify-center">Coffee shop</div>;
+  return (
+    <div className="flex items-center justify-center">
+      <div>
+        <Routes>
+          <Route path="/shops" element={<Shops />} />
+        </Routes>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/Shops/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Shops = () => {
+  return <div>Shops</div>;
+};
+
+export default Shops;
