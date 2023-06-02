967184bb535382347e1b2e23fa445b4211635a15
Nguyen Duong <muoi07052001@gmail.com>
2023-06-02 17:05:24 +0700

    Add data models

--- src/data/Shop.ts
+++ src/data/Shop.ts
@@ -1,10 +1,11 @@
 export const DummyShop = {
-  id: 1,
+  id: "6479a2e8d9adbede6db627dc",
   name: 'Shop',
-  status: 0, /// 0, 1, 2
   phone: '0339410721',
   city: 'Ha Noi',
   district: 'Hoang Mai',
   email: 'shop1@gmail.com',
   avatar: 'https://image.jpg',
+  password: 'password',
+  traffic: 1
 };
--- src/models/Shop.ts
+++ src/models/Shop.ts
@@ -1,12 +1,13 @@
-type statusEnum = 0 | 1 | 2;
+export type ShopTrafficType = 0 | 1 | 2 | 3 | 4 | 5; // 0(few), 1(moderate), 2(a bit crowded), 3(crowded), 4(out of seats), 5(closed)
 
 export interface ShopModel {
-  id: number;
+  id: string;
   name: string;
-  status: statusEnum; /// 0, 1, 2
   phone: string;
   city: string;
   district: string;
   email: string;
-  avatar: string;
+  avatar?: string;
+  password?: string;
+  traffic: ShopTrafficType;
 }
