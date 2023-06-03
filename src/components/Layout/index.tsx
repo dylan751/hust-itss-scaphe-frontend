19b344389499944396e89ee4c630de24cddb92f3
mdtthanh <maithanh0131@gmail.com>
2023-06-03 21:36:27 +0700

    fix layout

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import Navbar from '../Navbar';
+import Footer from '../Footer';
+import { Container, Grid } from '@mui/material';
 
-const Layout = () => {
-  return <div>Layout</div>;
+interface Props {
+  children: React.ReactNode;
+}
+const Layout = (props: Props) => {
+  return (
+    <>
+      <Navbar />
+      <Grid
+        sx={{
+          color: 'black',
+          fontWeight: 800,
+          fontSize: '25px',
+        }}
+      >
+        <Container sx={{ maxWidth: '70%' }}>
+          <Grid
+            sx={{
+              justifyContent: 'space-between',
+              alignItems: 'center',
+            }}
+          >
+            {props.children}
+          </Grid>
+        </Container>
+      </Grid>
+
+      <Footer />
+    </>
+  );
 };
 
 export default Layout;
